70d5d50f667d3a02ba8836000997b103294e3016
imnadsa <antosh1109@mail.ru>
2025-12-13 21:53:56 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import Info from "@/components/Info";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <Hero />
-      
-      {/* Здесь будут другие секции */}
-      <section className="py-20 text-center">
-        <div className="container-custom">
-          <h2 className="text-4xl font-bold mb-4">Другие секции будут здесь</h2>
-          <p className="text-gray-600">Специализация, статистика, варианты лечения, и т.д.</p>
-        </div>
-      </section>
+      <Info />
     </main>
   );
 }
