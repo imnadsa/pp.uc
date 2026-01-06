4058493aff98c2b6d48741dbba01050b0c36cf41
imnadsa <antosh1109@mail.ru>
2026-01-06 18:05:13 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Bonuses from '@/components/Bonuses';
 import Results from '@/components/Results';
 import Reviews from '@/components/Reviews';
 import Doctors from '@/components/Doctors';
-import finalcta from '@/components/finalcta';
+import Finalcta from '@/components/Finalcta';
 import Footer from '@/components/Footer';
 
 export default function Home() {
@@ -18,18 +18,18 @@ export default function Home() {
       <Header />
       <Hero />
       <Info />
-      <finalcta />
+      <Finalcta />
       <Benefits />
       <Appointment />
       <Safety />
       <Science />
-      <finalcta />
+      <Finalcta />
       <Bonuses />
       <Doctors />
       <Results />
       <Reviews />
       <Footer />
-      <finalcta />
+      <Finalcta />
     </main>
   );
 }
