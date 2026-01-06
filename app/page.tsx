243be6f58d926b232ce88728834b36c1ad14580c
imnadsa <antosh1109@mail.ru>
2026-01-06 17:18:44 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Info from "@/components/Info";
 import Benefits from "@/components/Benefits";
-import Science from '@/components/Science';
 import Appointment from '@/components/Appointment';
+import Science from '@/components/Science';
 import Results from '@/components/Results';
 import Reviews from '@/components/Reviews';
 import Savings from '@/components/Savings';
@@ -17,8 +17,8 @@ export default function Home() {
       <Hero />
       <Info />
       <Benefits />
-      <Science />
       <Appointment />
+      <Science />
       <Savings />
       <Doctors />
       <Results />
