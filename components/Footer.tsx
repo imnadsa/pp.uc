c8c6905a94c00e580d8bc7c77c403901eeb617b3
imnadsa <antosh1109@mail.ru>
2026-01-09 16:33:54 +0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,27 +14,11 @@ export default function Footer() {
   const clinics = [
     {
       city: "Санкт-Петербург",
-      address: "м. Международная, ул. Турку, д. 11, к. 2",
-      phones: ['8 (812) 320-10-01', '8 (812) 214-99-00'],
-      email: "office@ppclinic.ru",
-      mapSrc: "https://yandex.ru/map-widget/v1/?ll=30.379465%2C59.866573&mode=search&oid=1321972654&ol=biz&z=16.95",
-      image: "/turku.png"
-    },
-    {
-      city: "Санкт-Петербург",
       address: "м. Чкаловская, ул. Петрозаводская, д. 13",
       phones: ['8 (812) 320-10-01', '8 (812) 214-99-00'],
       email: "info@ppclinic.ru",
       mapSrc: "https://yandex.ru/map-widget/v1/?ll=30.295356%2C59.963685&mode=poi&poi%5Bpoint%5D=30.288881%2C59.964874&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D87260402616&z=16.45",
       image: "/petrozavodskaya.png"
-    },
-    {
-      city: "Гатчина",
-      address: "ул. Коли Подрядчикова д. 22",
-      phones: ['8 (812) 320-10-01', '8 (812) 214-99-00'],
-      email: "info_ppg@ppclinic.ru",
-      mapSrc: "https://yandex.ru/map-widget/v1/?ll=30.121839%2C59.580181&mode=search&oid=139473820804&ol=biz&z=16.95",
-      image: "/gatchina.png"
     }
   ];
 
