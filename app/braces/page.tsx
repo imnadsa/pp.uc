36d75d555b322fc6a62fb39facc87e788e553247
imnadsa <antosh1109@mail.ru>
2026-01-17 19:25:17 +0400

    Update page.tsx

--- app/braces/page.tsx
+++ app/braces/page.tsx
@@ -3,6 +3,7 @@ import HeroBraces from '@/components/braces/HeroBraces';
 import Footer from '@/components/Footer';
 import Section2 from '@/components/braces/Section2';
 import Section3 from '@/components/braces/Section3';
+import Section4 from '@/components/braces/Section4';
 import ModalProvider from '@/components/ModalProvider';
 
 export default function BracesPage() {
