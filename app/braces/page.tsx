831ba8434e6ad07d5e2ad38710ee7553e83e0e72
imnadsa <antosh1109@mail.ru>
2026-01-17 20:09:36 +0400

    Update page.tsx

--- app/braces/page.tsx
+++ app/braces/page.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/Footer';
 import Section2 from '@/components/braces/Section2';
 import Section3 from '@/components/braces/Section3';
 import Section4 from '@/components/braces/Section4';
+import MultiStepForm from '@/components/braces/MultiStepForm';
 import ModalProvider from '@/components/ModalProvider';
 
 export default function BracesPage() {
@@ -15,6 +16,7 @@ export default function BracesPage() {
         <Section2 />
         <Section3 />
         <Section4 />
+        <MultiStepForm />
         {/* Здесь можно добавить другие блоки */}
         <Footer />
       </main>
