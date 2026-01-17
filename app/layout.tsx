771303c48c7e92eefcf02b0c8fe49367787aa945
imnadsa <antosh1109@mail.ru>
2026-01-17 14:16:45 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -76,6 +77,37 @@ export default function RootLayout({
     <html lang="ru">
       <body>
         {children}
+        
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106305816', 'ym');
+            
+            ym(106305816, 'init', {
+              ssr:true,
+              webvisor:true,
+              clickmap:true,
+              ecommerce:"dataLayer",
+              accurateTrackBounce:true,
+              trackLinks:true
+            });
+          `}
+        </Script>
+        
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/106305816" 
+              style={{position: 'absolute', left: '-9999px'}} 
+              alt="" 
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
