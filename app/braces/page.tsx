0ab87b3a0c63c2ce715fa7125ccf58b63ed0eb3d
imnadsa <antosh1109@mail.ru>
2026-01-17 19:26:27 +0400

    Update page.tsx

--- app/braces/page.tsx
+++ app/braces/page.tsx
@@ -14,6 +14,7 @@ export default function BracesPage() {
         <HeroBraces />
         <Section2 />
         <Section3 />
+        <Section4 >
         {/* Здесь можно добавить другие блоки */}
         <Footer />
       </main>
