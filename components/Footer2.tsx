8e8c22dd021cf3ea3832549236d56d437c902b7c
imnadsa <antosh1109@mail.ru>
2026-02-03 18:06:58 +0400

    Update Footer2.tsx

--- components/Footer2.tsx
+++ components/Footer2.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
     {
       city: "Санкт-Петербург",
       address: "м. Международная, ул. Турку, д. 11, к. 2",
-      phones: ['8 (812) 320-10-01', '8 (812) 214-99-00'],
+      phones: ['+7 (812) 421-69-28'],
       email: "office@ppclinic.ru",
       mapSrc: "https://yandex.ru/map-widget/v1/?ll=30.379465%2C59.866573&mode=search&oid=1321972654&ol=biz&z=16.95",
       image: "/turku.png"
@@ -23,7 +23,7 @@ export default function Footer() {
     {
       city: "Санкт-Петербург",
       address: "м. Чкаловская, ул. Петрозаводская, д. 13",
-      phones: ['8 (812) 320-10-01', '8 (812) 214-99-00'],
+      phones: ['+7 (812) 421-69-28'],
       email: "info@ppclinic.ru",
       mapSrc: "https://yandex.ru/map-widget/v1/?ll=30.295356%2C59.963685&mode=poi&poi%5Bpoint%5D=30.288881%2C59.964874&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D87260402616&z=16.45",
       image: "/petrozavodskaya.png"
@@ -31,7 +31,7 @@ export default function Footer() {
     {
       city: "Гатчина",
       address: "ул. Коли Подрядчикова д. 22",
-      phones: ['8 (812) 320-10-01', '8 (812) 214-99-00'],
+      phones: ['+7 (812) 421-69-28'],
       email: "info_ppg@ppclinic.ru",
       mapSrc: "https://yandex.ru/map-widget/v1/?ll=30.121839%2C59.580181&mode=search&oid=139473820804&ol=biz&z=16.95",
       image: "/gatchina.png"
@@ -118,7 +118,7 @@ export default function Footer() {
                   
                   <div className="mt-auto space-y-3 pt-5 border-t border-gray-700">
                     {clinic.phones.map(phone => (
-                      <a key={phone} href={`tel:${phone.replace(/\D/g, '')}`} className="block text-gray-300 font-bold hover:text-white transition-colors text-lg">
+                      <a key={phone} href="tel:+78124216928" className="block text-gray-300 font-bold hover:text-white transition-colors text-lg">
                         {phone}
                       </a>
                     ))}
